b2830c7666ea4f750edf20a52645bc87de52d9f6
ve1ikiy <ve1ikiy.work@gmail.com>
2017-04-07 16:41:16 +0300

    Немного контента, чтобы понять в какую сторону двигаться дальше

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
-	gulp.watch('app/sass/**/*.scss', ['sass']);
+	gulp.watch('app/scss/**/*.scss', ['sass']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
 	gulp.watch('app/*.html', browserSync.reload);
 });
